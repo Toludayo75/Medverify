bb591b6e431ed8722f6e0e32f7edcad0927c77fe
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-06-20 12:31:29 +0100

    first tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./client/index.html", "./client/src/**/*.{js,jsx,ts,tsx}"],
+    content: [
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+      "./client/index.html"
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       borderRadius: {
