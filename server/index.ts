acacf05269266ca5769a95293f45c5c8ece630cf
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-06-20 11:11:41 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -56,10 +56,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
-  const port = 3000;
+  // Use the port assigned by Render or fallback to 3000 locally
+  const port = Number(process.env.PORT) || 3000;
+
   server.listen({
     port,
     host: "0.0.0.0",
