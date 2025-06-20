05a7b2d90f239f634b12ceb4d0b53f6733ac22d7
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-06-20 11:25:12 +0100

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -6,9 +6,12 @@ import { setupAuth } from "./auth";
 import { insertDrugSchema, insertBatchSchema, insertReportSchema } from "@shared/schema";
 
 export async function registerRoutes(app: Express): Promise<Server> {
+    app.get("/", (_req, res) => {
+    res.send("Medverify API is running");
+  });
   // API route to handle PDF download
   app.get("/api/downloads/safety-guidelines", (req, res) => {
-    // Generate a simple PDF response
+  // Generate a simple PDF response
     res.setHeader('Content-Type', 'application/pdf');
     res.setHeader('Content-Disposition', 'attachment; filename=MedVerify-Drug-Safety-Guidelines.pdf');
     
