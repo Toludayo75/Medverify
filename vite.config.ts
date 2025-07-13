422d513993f78e6e7b7ea0a5faa9b55d9e36ab78
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-07-13 20:18:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  root: path.resolve(__dirname, "client"),  // <---- Add this line back
+  root: path.resolve(__dirname, "client"),
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
+      "hooks": path.resolve(__dirname, "client/src/hooks"),
       "@shared": path.resolve(__dirname, "shared"),
     },
     extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
   },
 });
-
